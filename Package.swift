9e1948c6597ef890f51edfe8279601718763c7b8
Brian Floersch <gh123man@gmail.com>
2023-09-21 17:13:06 -0400

    Add vision OS support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Refresher",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -16,13 +17,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftUIIntrospect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "1.0.0"),
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Refresher",
-            dependencies: ["SwiftUIIntrospect"]),
+            dependencies: [
+                .product(
+                    name: "SwiftUIIntrospect",
+                    package: "SwiftUI-Introspect")
+            ]
+        ),
     ]
 )
